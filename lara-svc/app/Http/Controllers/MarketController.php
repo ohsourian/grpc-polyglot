e285e233a2c65841b974f5978b913ded63173b7d
ohsourian <ohsourian.mgr@gmail.com>
2022-06-20 20:59:58 +0900

    add laravel php grpc config

--- lara-svc/app/Http/Controllers/MarketController.php
+++ lara-svc/app/Http/Controllers/MarketController.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
+namespace App\gRPC\HelloRequest;
 
 class MarketController extends Controller
 {
     public function greetUser()
     {
+
         return response()->json("foo!");
     }
 }
